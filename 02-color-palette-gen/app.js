852d04381d38b00bcd4a2e2cce07df611be96e98
srujan <srujan.sharma77k@gmail.com>
2023-01-26 15:03:53 +1100

    some progress made

--- 02-color-palette-gen/app.js
+++ 02-color-palette-gen/app.js
@@ -6,6 +6,11 @@ const currentHexes = document.querySelectorAll(".color h2");
 
 let initialColors;
 
+// event listener
+sliders.forEach((slider) => {
+  slider.addEventListener("input", hslControls);
+});
+
 // generate a random hex color using chroma.js
 function generateHex() {
   const hexColor = chroma.random(); // generates random hex color
@@ -22,6 +27,38 @@ function checkTextContrast(color, text) {
   }
 }
 
+// hsl controls
+function hslControls(e) {
+  const index =
+    e.target.getAttribute("data-bright") ||
+    e.target.getAttribute("data-saturation") ||
+    e.target.getAttribute("data-hue");
+
+  let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
+  console.log(sliders);
+}
+
+// colorize sliders
+function colorizeSliders(color, hue, brightness, saturation) {
+  // saturation scale
+  const noSat = color.set("hsl.s", 0);
+  const fullSat = color.set("hsl.s", 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+  // brightness scale
+  const midBright = color.set("hsl.l", 0.5);
+  const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+  // update input color
+  saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(
+    0
+  )}, ${scaleSat(1)})`;
+  brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(
+    0
+  )},${scaleBright(0.5)}, ${scaleBright(1)})`;
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204), rgb(204,75,75))`;
+}
+
 // displaying randomColors
 function randomColors() {
   colorDivs.forEach((div) => {
@@ -33,6 +70,14 @@ function randomColors() {
     hexText.innerText = randomColor;
     // contrast check
     checkTextContrast(randomColor, hexText);
+    // Initial colorize sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+    console.log(saturation);
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
 
